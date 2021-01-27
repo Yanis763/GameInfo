9cb4e4a6c59b51c86ffb0c68064c14cc98f6acda
Yanis763 <yanispsn76@hotmail.com>
2021-01-27 13:40:12 +0100

    first commit

---
+++ src/js/PageList.js
@@ -0,0 +1,41 @@
+const PageList = (argument = "") => {
+  console.log("Page List", argument);
+    const preparePage = () => {
+        cleanedArgument = argument.replace(/\s+/g, "-");
+        let articles = "";
+    
+        const fetchList = (url, argument) => {
+          let finalURL = url;
+          if (argument) {
+            finalURL = url + "?search=" + argument;
+          }
+    
+          fetch(`${finalURL}`)
+            .then((response) => response.json())
+            .then((data) => {
+              data.results.forEach((article) => {
+                articles += `
+                      <div class="cardGame">
+                        <h1>${article.name}</h1>
+                        <h2>${article.released}</h2>
+                        <a href = "#pagedetail/${article.id}">${article.id}</a>
+                      </div>
+                    `;
+              });
+              document.querySelector(".page-list .articles").innerHTML = article;
+            });
+        };
+    
+        fetchList("https://api.rawg.io/api/games", cleanedArgument);
+      };
+    
+    const render = () => {
+        pageContent.innerHTML = `
+          <section class="page-list">
+            <div class="articles">Hey, this page is a PageList template, about : ${argument}</div>
+          </section>
+        `;
+        preparePage();
+      };
+      render();
+};
---
+++ src/js/routes.js
@@ -0,0 +1,13 @@
+import { Home } from "./Home";
+import { PageDetail } from "./PageDetail";
+import { PageList } from "./PageList"
+
+
+const routes = {
+    "": Home,
+    "pagelist": PageList,
+    "pagedetail": PageDetail,
+};
+
+
+
